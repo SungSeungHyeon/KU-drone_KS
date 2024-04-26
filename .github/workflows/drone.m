db49fd24220f0ffe2b080ac293e760b9b62a6fb2
Kojuyoung-22 <130830666+Kojuyoung-22@users.noreply.github.com>
2024-04-26 19:00:38 +0900

    Update drone.m

--- .github/workflows/drone.m
+++ .github/workflows/drone.m
@@ -2,5 +2,5 @@ a = input("Enter a time: ")
 t1 = datetime('now');
 t2 = t1 + hours(a)
 
-t2.Fromat = 'yyyy-MM-dd hh:mm:ss'
+t2.Format = 'yyyy-MM-dd hh:mm:ss'
 fprintf("%d hours from the current time, it is %s\n",a, t2)
